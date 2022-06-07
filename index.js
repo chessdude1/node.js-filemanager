0df9e19fe7a332c24b162731afcafb43adab65d6
chessdude1 <mitia.platonow2011@yandex.ru>
2022-06-07 20:18:56 +0300

    feat: add main stream

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log(1)
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function mainStream() {
+
+
+  rl.question(`What's your name? \n`, name => {
+    console.log(`Hi ${name}!`);
+  });
+
+  rl.on('line', (line) => {
+    console.log(line)
+  })
+
+
+}
+
+mainStream()
+
